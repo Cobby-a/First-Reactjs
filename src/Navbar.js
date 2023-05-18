28b290fbe31ba961a327e4c53c0a68b9910ed008
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-18 13:12:48 +0000

    added Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+
+const Navbar = () =>{
+    return(
+        <nav>
+            <h1>The Dojo Blog</h1>
+            <div>
+                <a href="google.com">HomeNew Blog</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
