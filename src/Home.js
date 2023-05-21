62196fded6eaff5ba783d95f894d825ae2f35fd7
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-21 21:55:26 +0000

    created the home page

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
+import { useState } from "react"
+import BlogList from "./BlogList";
+
 
 const Home = () => {
+    const[blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'uhuhrwiuhiurhiw ...', author: 'yueghjbeewiuu', id: 1},
+        {title: 'Welcome party!', body: 'huehfuweh ...', author: 'iuhuuhiu', id: 2},
+        {title: 'Web dev top tips', body: 'give me ...', author:"yuiuewi", id: 3}
+    ]);
     return(
-        <div>
-            <h2>Homepage</h2>
+        <div className="home">
+            <BlogList blogs = {blogs} title="All Blogs!"/>
         </div>
     )
 }
