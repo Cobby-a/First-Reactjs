8a4ac02e8badb5fa18449d0b8c9ccd7c47e421de
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-22 23:25:26 +0000

    updated props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,5 @@
 
 const BlogList =({title, blogs, handleClick})=>{
-    // const blogs = props.blogs;
-    // const title = props.title;
     return(
         <div className="blog-list">
             <h2>{title}</h2>
