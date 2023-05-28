924a21c6db52ec966c9504e2b9a45371abd6e987
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-28 23:02:54 +0000

    removed the handleClick for the delete blog

--- src/Home.js
+++ src/Home.js
@@ -6,10 +6,6 @@ const Home = () => {
     const[blogs, setBlogs] = useState(null);
 
 const [name, setName] = useState('Emma')
-const handleClick = (id) => {
-    const del = blogs.filter((blog)=>blog.id !==id)
-    setBlogs(del)
-}
 useEffect (()=>{
     fetch('http://localhost:4000/blogs')
         .then(response =>{
@@ -21,7 +17,7 @@ useEffect (()=>{
 },[])
     return(
         <div className="home">
-            {blogs && <BlogList blogs = {blogs} title="All Blogs!" handleClick={handleClick}/>}
+            {blogs && <BlogList blogs = {blogs} title="All Blogs!"/>}
             <button onClick={()=>setName('Cobby')}>Change name</button>
             <p>{name}</p>
             {/* <BlogList blogs = {blogs.filter((blog)=>(blog.author==='mario'))} title="Mario's Blogs"/> */}
