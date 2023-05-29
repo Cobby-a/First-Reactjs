89f80739dc692a269ab44f1e5b48fcbc0286b1bb
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-29 12:42:54 +0000

    to call use effect and state from the custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,13 @@
-import { useEffect, useState } from "react"
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 
 const Home = () => {
-    const[blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const[error, setError] = useState(null);
+
+    const{data: blogs, error, isPending} = useFetch('http://localhost:4000/blogs')
 
 // const [name, setName] = useState('Emma')
-useEffect (()=>{
-    setTimeout(()=>{fetch('http://localhost:4000/blogss')
-        .then(response =>{
-            if(!response.ok){
-                throw Error("couldn't load your resources due to some error, try again")
-            }
-            return response.json();
-        })
-        .then(data =>{
-            setBlogs(data)
-            setIsPending(false)
-            setError(null);
-        })
-        .catch(err =>{
-            setBlogs(null)
-            setIsPending(false);
-            setError(err.message);
-        });
-},1000);
-},[])
+
     return(
         <div className="home">
             {error && <div>{error}</div>}
