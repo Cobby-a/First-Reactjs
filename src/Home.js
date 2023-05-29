d7fe23d2438ad46b2ed2fb34a3431c765be8dfc3
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-29 11:31:51 +0000

    added a conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const[blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
-const [name, setName] = useState('Emma')
+// const [name, setName] = useState('Emma')
 useEffect (()=>{
-    fetch('http://localhost:4000/blogs')
+    setTimeout(()=>{fetch('http://localhost:4000/blogs')
         .then(response =>{
             return response = response.json();
         })
         .then(data =>{
             setBlogs(data)
+            setIsPending(false)
         });
+},1000);
 },[])
     return(
         <div className="home">
+            {isPending && <div>is Loading ...</div>}
             {blogs && <BlogList blogs = {blogs} title="All Blogs!"/>}
-            <button onClick={()=>setName('Cobby')}>Change name</button>
-            <p>{name}</p>
+            {/* <button onClick={()=>setName('Cobby')}>Change name</button>
+            <p>{name}</p> */}
             {/* <BlogList blogs = {blogs.filter((blog)=>(blog.author==='mario'))} title="Mario's Blogs"/> */}
         </div>
     )
