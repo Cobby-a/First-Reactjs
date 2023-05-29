60ec43279a0149e3323d3de061985763fc721128
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-05-29 12:03:09 +0000

    modified the catch error

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,32 @@ import BlogList from "./BlogList";
 const Home = () => {
     const[blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);
 
 // const [name, setName] = useState('Emma')
 useEffect (()=>{
-    setTimeout(()=>{fetch('http://localhost:4000/blogs')
+    setTimeout(()=>{fetch('http://localhost:4000/blogss')
         .then(response =>{
-            return response = response.json();
+            if(!response.ok){
+                throw Error("couldn't load your resources due to some error, try again")
+            }
+            return response.json();
         })
         .then(data =>{
             setBlogs(data)
             setIsPending(false)
+            setError(null);
         })
         .catch(err =>{
-            console.log(err.message)
+            setBlogs(null)
+            setIsPending(false);
+            setError(err.message);
         });
 },1000);
 },[])
     return(
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>is Loading ...</div>}
             {blogs && <BlogList blogs = {blogs} title="All Blogs!"/>}
             {/* <button onClick={()=>setName('Cobby')}>Change name</button>
