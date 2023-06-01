0a715e6b8c2c2f21bbf6d358af7f940862d395a2
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-06-01 12:36:26 +0000

    modified our app to use the v6 of react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar />
       <div className='content'>
-        <Switch>
-          <Route path ="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path ="/" element={<Home/>}/>
+        </Routes>
       </div>
     </div>
     </BrowserRouter>
