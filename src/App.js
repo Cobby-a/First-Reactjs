de5f5dcd0c94abf09b8fb4a630cb82830a305178
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-06-01 12:33:36 +0000

    added the react router components to our app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar />
       <div className='content'>
-        <Home />
+        <Switch>
+          <Route path ="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
