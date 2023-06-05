6cc90edc45c205edae2e2b6127b7b1ea37927851
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-06-05 11:36:04 +0000

    made a POST request to our form

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,19 @@ const Create = ()=>{
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
+    const [isPending, setISPending] = useState(false)
     const handleSubmit = (e) => {
         e.preventDefault();
+        setISPending(true)
         const blog = {title, body, author}
-        console.log(blog)
+        fetch ('http://localhost:4000/blogs', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('New Blog Added')
+            setISPending(false);
+        })
     }
     return(
         <div className="create">
@@ -37,7 +46,8 @@ const Create = ()=>{
                     <option value="Cobby">Cobby</option>
                     <option value="Yaa">Yaa</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
                 <p>Title: {title}</p>
                 <p>{body}</p>
                 <p>By: {author}</p>
